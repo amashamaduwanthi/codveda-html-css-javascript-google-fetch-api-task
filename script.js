5808940c93e18a5b2c6e69e43ff8019bfa185373
amashamaduwanthi <amashamaduwanthi123@gmail.com>
2025-05-07 13:52:02 +0530

    Add fetch logic to retrieve and display books from Google Books API

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function searchBooks(){
    const query=document.getElementById('searchInput').ariaValueMax;
    const url=`https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(query)}`;
+
+   fetch(url)
+   .then(response => response.json())
+   .then(data =>displayBooks(data.items))
+   .catch(error => console.error("Error fetching books:", error));
 }
