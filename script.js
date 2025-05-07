84dc79a71c371d0292f3f31662ebb84c0d59dee0
amashamaduwanthi <amashamaduwanthi123@gmail.com>
2025-05-07 14:09:37 +0530

    validate the text field

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 function searchBooks(){
-   const query=document.getElementById('searchInput').ariaValueMax;
+   const query=document.getElementById('searchInput').value;
    const url=`https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(query)}`;
 
    fetch(url)
    .then(response => response.json())
    .then(data =>displayBooks(data.items))
    .catch(error => console.error("Error fetching books:", error));
+}
+
+function displayBooks(books) {
+    const bookList = document.getElementById("book-list");
+    if (!books || books.length === 0) {
+      bookList.innerHTML = "<p>No books found.</p>";
+      return;
+    }
+  
 }
